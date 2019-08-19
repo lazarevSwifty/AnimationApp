512aad6a4bdac11f5b2f75ff59f68284b6f39741
Vlad <vladlazarevdev@mail.ru>
2019-08-20 00:07:03 +0300

    Initial commit

--- AnimationApp/ViewController.swift
+++ AnimationApp/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AnimationViewController.swift
 //  AnimationApp
 //
 //  Created by Владислав Лазарев on 19/08/2019.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AnimationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
